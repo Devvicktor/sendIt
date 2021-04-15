86818f66a81fa6e6313861896676dac8fcb74d26
Devvicktor <vkosgei001@gmail.com>
2021-04-15 09:50:23 +0300

    push

--- receiver.js
+++ receiver.js
@@ -1,90 +1,446 @@
-const web_socket = new WebSocket ('wss://sendithere.herokuapp.com/');
 
-web_socket.onmessage = e => {
-  function handleSignallingData (data) {
-    switch (data.type) {
-      case 'offer':
-        peerConnection.setRemoteDescription (data.offer);
-        createAndSendAnswer ();
-        break;
-      case 'candidate':
-        peerConnection.addIceCandidate (data.candidate);
-    }
+var myHostname = window.location.hostname;
+if (!myHostname) {
+  myHostname = "localhost";
+}
+log("Hostname: " + myHostname);
+
+// WebSocket chat/signaling channel variables.
+
+const connection = null;
+const clientID = 0;
+
+
+//could be declared on getUsermedia query
+var mediaConstraints = {
+  audio: true,            // We want an audio track
+  video: {
+    frameRate: 24,
+    width: {
+      min: 480,
+      max: 1280,
+      ideal: 720
+    },
+    aspectRatio: 1.333
   }
-  handleSignallingData (JSON.parse (e.data));
 };
 
-function sendData (data) {
-  data.username = username;
-  web_socket.send (JSON.stringify (data));
+var myUsername = null;
+var targetUsername = null;
+var myPeerConnection = null;
+var transceiver = null;
+var webcamStream = null;
+
+
+
+function log(text) {
+  var time = new Date();
+
+  console.log("[" + time.toLocaleTimeString() + "] " + text);
 }
-let localStream;
-let username;
-let peerConnection;
-function joinCall () {
- username = document.getElementById ('join-input').value;
-  document.getElementById ('video-call-div').style.display = 'inline';
-  navigator.getUserMedia =
-    navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia;
-  if (navigator.getUserMedia) {
-    navigator.getUserMedia (
-      {
-        video: {
-          frameRate: 24,
-          width: {
-            min: 480,
-            max: 1280,
-            ideal: 720,
-          },
-          aspectRatio: 1.333,
-        },
-        audio: true,
-      },
-      stream => {
-        localStream = stream;
-        document.getElementById ('local_video').srcObject = localStream;
-        let configuration = {
-          iceServers: [
-            {
-              url: 'stun:stun2.l.google.com:19302',
-            },
-          ],
-        };
-        peerConnection = new RTCPeerConnection (configuration);
-        peerConnection.addEventListener("addstream", ev => doAddStream(ev.localStream), false);
-        peerConnection.onaddstream = e => {
-          document.getElementById ('remote_video').srcObject = e.stream;
-        };
-        peerConnection.onicecandidate = e => {
-          if (e.candidate) 
-          return;
-          sendData ({
-            type: 'send_candidate',
-            candidate: e.candidate,
-          });
-        };
-        sendData ({
-          type: 'join_call',
-        });
-      },
-      error => {
-        console.log (error);
-      }
-    );
-  } else {
-    console.log ('getUserMedia not supported');
+function log_error(text) {
+  var time = new Date();
+
+  console.trace("[" + time.toLocaleTimeString() + "] " + text);
+}
+function sendToServer(msg) {
+  var msgJSON = JSON.stringify(msg);
+
+  log("Sending '" + msg.type + "' message: " + msgJSON);
+  connection.send(msgJSON);
+}
+function setUsername() {
+  myUsername = document.getElementById("name").value;
+
+  sendToServer({
+    name: myUsername,
+    date: Date.now(),
+    id: clientID,
+    type: "username"
+  });
+}
+function connect() {
+  var serverUrl;
+  var scheme = "ws";
+  if (document.location.protocol === "https:") {
+    scheme += "s";
+  }
+  serverUrl = scheme + "://" + myHostname + ":3000";
+
+  log(`Connecting to server: ${serverUrl}`);
+  connection = new WebSocket(serverUrl, "json");
+
+  connection.onopen = function(evt) {
+    document.getElementById("text").disabled = false;
+    document.getElementById("send").disabled = false;
+  };
+
+  connection.onerror = function(evt) {
+    console.dir(evt);
+  }
+
+  connection.onmessage = function(evt) {
+    var chatBox = document.querySelector(".chatbox");
+    var text = "";
+    var msg = JSON.parse(evt.data);
+    log("Message received: ");
+    console.dir(msg);
+    var time = new Date(msg.date);
+    var timeStr = time.toLocaleTimeString();
+
+    switch(msg.type) {
+      case "id":
+        clientID = msg.id;
+        setUsername();
+        break;
+
+      case "username":
+        text = "<b>User <em>" + msg.name + "</em> signed in at " + timeStr + "</b><br>";
+        break;
+
+      case "message":
+        text = "(" + timeStr + ") <b>" + msg.name + "</b>: " + msg.text + "<br>";
+        break;
+
+      case "rejectusername":
+        myUsername = msg.name;
+        text = "<b>Your username has been set to <em>" + myUsername +
+          "</em> because the name you chose is in use.</b><br>";
+        break;
+
+      case "userlist":
+        handleUserlistMsg(msg);
+        break;
+      case "video-offer":
+        handleVideoOfferMsg(msg);
+        break;
+
+      case "video-answer":
+        handleVideoAnswerMsg(msg);
+        break;
+
+      case "new-ice-candidate":
+        handleNewICECandidateMsg(msg);
+        break;
+
+      case "hang-up":
+        handleHangUpMsg(msg);
+        break;
+
+
+
+      default:
+        log_error("Unknown message received:");
+        log_error(msg);
+    }
+
+    if (text.length) {
+      chatBox.innerHTML += text;
+      chatBox.scrollTop = chatBox.scrollHeight - chatBox.clientHeight;
+    }
+  };
+}
+function handleSendButton() {
+  var msg = {
+    text: document.getElementById("text").value,
+    type: "message",
+    id: clientID,
+    date: Date.now()
+  };
+  sendToServer(msg);
+  document.getElementById("text").value = "";
+}
+function handleKey(evt) {
+  if (evt.keyCode === 13 || evt.keyCode === 14) {
+    if (!document.getElementById("send").disabled) {
+      handleSendButton();
+    }
   }
 }
-let isAudio = true;
-function muteAudio () {
-  isAudio = !isAudio;
-  localStream.getAudioTracks ()[0].enabled = isAudio;
+
+async function createPeerConnection() {
+  log("Setting up a connection...");
+
+
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [     // Information about ICE servers - Use your own!
+      {
+        urls: "turn:" + myHostname,  // A TURN server
+        username: "webrtc",
+        credential: "turnserver"
+      }
+    ]
+  });
+  myPeerConnection.onicecandidate = handleICECandidateEvent;
+  myPeerConnection.oniceconnectionstatechange = handleICEConnectionStateChangeEvent;
+  myPeerConnection.onicegatheringstatechange = handleICEGatheringStateChangeEvent;
+  myPeerConnection.onsignalingstatechange = handleSignalingStateChangeEvent;
+  myPeerConnection.onnegotiationneeded = handleNegotiationNeededEvent;
+  myPeerConnection.ontrack = handleTrackEvent;
 }
-let isVideo = true;
-function muteVideo () {
-  isvideo = !isVideo;
-  localStream.getVideoTrack ()[0] = isVideo;
+
+async function handleNegotiationNeededEvent() {
+  log("*** Negotiation needed");
+
+  try {
+    log("---> Creating offer");
+    const offer = await myPeerConnection.createOffer();
+
+    if (myPeerConnection.signalingState != "stable") {
+      log("     -- The connection isn't stable yet; postponing...")
+      return;
+    }
+    log("---> Setting local description to the offer");
+    await myPeerConnection.setLocalDescription(offer);
+
+
+    log("---> Sending the offer to the remote peer");
+    sendToServer({
+      name: myUsername,
+      target: targetUsername,
+      type: "video-offer",
+      sdp: myPeerConnection.localDescription
+    });
+  } catch(err) {
+    log("*** The following error occurred while handling the negotiationneeded event:");
+    reportError(err);
+  };
+}
+
+function handleTrackEvent(event) {
+  log("*** Track event");
+  document.getElementById("remote_video").srcObject = event.streams[0];
+  document.getElementsByClassName("hangup-button").disabled = false;
+}
+
+function handleICECandidateEvent(event) {
+  if (event.candidate) {
+    log("*** Outgoing ICE candidate: " + event.candidate.candidate);
+
+    sendToServer({
+      type: "new-ice-candidate",
+      target: targetUsername,
+      candidate: event.candidate
+    });
+  }
+}
+function handleICEConnectionStateChangeEvent(event) {
+  log("*** ICE connection state changed to " + myPeerConnection.iceConnectionState);
+
+  switch(myPeerConnection.iceConnectionState) {
+    case "closed":
+    case "failed":
+    case "disconnected":
+      closeVideoCall();
+      break;
+  }
+}
+
+function handleSignalingStateChangeEvent(event) {
+  log("*** WebRTC signaling state changed to: " + myPeerConnection.signalingState);
+  switch(myPeerConnection.signalingState) {
+    case "closed":
+      closeVideoCall();
+      break;
+  }
+}
+function handleICEGatheringStateChangeEvent(event) {
+  log("*** ICE gathering state changed to: " + myPeerConnection.iceGatheringState);
+}
+
+function handleUserlistMsg(msg) {
+  var i;
+  var listElem = document.querySelector(".userlistbox");
+
+  while (listElem.firstChild) {
+    listElem.removeChild(listElem.firstChild);
+  }
+
+  msg.users.forEach(function(username) {
+    var item = document.createElement("li");
+    item.appendChild(document.createTextNode(username));
+    item.addEventListener("click", invite, false);
+
+    listElem.appendChild(item);
+  });
+}
+
+function closeVideoCall() {
+  var localVideo = document.getElementById("local_video");
+
+  log("Closing the call");
+
+
+
+  if (myPeerConnection) {
+    log("--> Closing the peer connection");
+
+    myPeerConnection.ontrack = null;
+    myPeerConnection.onnicecandidate = null;
+    myPeerConnection.oniceconnectionstatechange = null;
+    myPeerConnection.onsignalingstatechange = null;
+    myPeerConnection.onicegatheringstatechange = null;
+    myPeerConnection.onnotificationneeded = null;
+    myPeerConnection.getTransceivers().forEach(transceiver => {
+      transceiver.stop();
+    });
+    if (localVideo.srcObject) {
+      localVideo.pause();
+      localVideo.srcObject.getTracks().forEach(track => {
+        track.stop();
+      });
+    }
+
+    myPeerConnection.close();
+    myPeerConnection = null;
+    webcamStream = null;
+  }
+
+  document.getElementById("hangup-button").disabled = true;
+  targetUsername = null;
+}
+
+function handleHangUpMsg(msg) {
+  log("*** Received hang up notification from other peer");
+
+  closeVideoCall();
+}
+function hangUpCall() {
+  closeVideoCall();
+
+  sendToServer({
+    name: myUsername,
+    target: targetUsername,
+    type: "hang-up"
+  });
+}
+async function invite(evt) {
+  log("Starting to prepare an invitation");
+  if (myPeerConnection) {
+    alert("You can't start a call because you already have one open!");
+  } else {
+    var clickedUsername = evt.target.textContent;
+    if (clickedUsername === myUsername) {
+      alert("I'm afraid I can't let you talk to yourself. That would be weird.");
+      return;
+    }
+    targetUsername = clickedUsername;
+    log("Inviting user " + targetUsername);
+    log("Setting up connection to invite user: " + targetUsername);
+    createPeerConnection();
+
+    try {
+      webcamStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+      document.getElementById("local_video").srcObject = webcamStream;
+    } catch(err) {
+      handleGetUserMediaError(err);
+      return;
+    }
+
+
+
+    try {
+      webcamStream.getTracks().forEach(
+        transceiver = track => myPeerConnection.addTransceiver(track, {streams: [webcamStream]})
+      );
+    } catch(err) {
+      handleGetUserMediaError(err);
+    }
+  }
+}
+
+async function handleVideoOfferMsg(msg) {
+  targetUsername = msg.name;
+
+
+  log("Received video chat offer from " + targetUsername);
+  if (!myPeerConnection) {
+    createPeerConnection();
+  }
+
+
+
+  var desc = new RTCSessionDescription(msg.sdp);
+
+
+  if (myPeerConnection.signalingState != "stable") {
+    log("  - But the signaling state isn't stable, so triggering rollback");
+
+    await Promise.all([
+      myPeerConnection.setLocalDescription({type: "rollback"}),
+      myPeerConnection.setRemoteDescription(desc)
+    ]);
+    return;
+  } else {
+    log ("  - Setting remote description");
+    await myPeerConnection.setRemoteDescription(desc);
+  }
+
+  if (!webcamStream) {
+    try {
+      webcamStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+    } catch(err) {
+      handleGetUserMediaError(err);
+      return;
+    }
+    document.getElementById("local_video").srcObject = webcamStream;
+    try {
+      webcamStream.getTracks().forEach(
+        transceiver = track => myPeerConnection.addTransceiver(track, {streams: [webcamStream]})
+      );
+    } catch(err) {
+      handleGetUserMediaError(err);
+    }
+  }
+
+  log("---> Creating and sending answer to caller");
+
+  await myPeerConnection.setLocalDescription(await myPeerConnection.createAnswer());
+
+  sendToServer({
+    name: myUsername,
+    target: targetUsername,
+    type: "video-answer",
+    sdp: myPeerConnection.localDescription
+  });
+}
+
+
+async function handleVideoAnswerMsg(msg) {
+  log("*** Call recipient has accepted our call");
+  var desc = new RTCSessionDescription(msg.sdp);
+  await myPeerConnection.setRemoteDescription(desc).catch(reportError);
+}
+async function handleNewICECandidateMsg(msg) {
+  var candidate = new RTCIceCandidate(msg.candidate);
+
+  log("*** Adding received ICE candidate: " + JSON.stringify(candidate));
+  try {
+    await myPeerConnection.addIceCandidate(candidate)
+  } catch(err) {
+    reportError(err);
+  }
+}
+function handleGetUserMediaError(e) {
+  log_error(e);
+  switch(e.name) {
+    case "NotFoundError":
+      alert("Unable to open your call because no camera and/or microphone" +
+            "were found.");
+      break;
+    case "SecurityError":
+    case "PermissionDeniedError":
+
+      break;
+    default:
+      alert("Error opening your camera and/or microphone: " + e.message);
+      break;
+  }
+
+
+
+  closeVideoCall();
+}
+function reportError(errMessage) {
+  log_error(`Error ${errMessage.name}: ${errMessage.message}`);
 }
--- sender.js
+++ sender.js
@@ -1,110 +1,317 @@
-const webSocket = new WebSocket('wss://sendithere.herokuapp.com/');
-webSocket.onmessage = (e) => {
-  handleSignallingData(JSON.parse(e.data))
+
+var https = require('https');
+var fs = require('fs');
+var WebSocketServer = require('websocket').server;
+
+
+// Used for managing the text chat user list.
+
+var connectionArray = [];
+var nextID = Date.now();
+var appendToMakeUnique = 1;
+//
+//our sevrver
+fs.readFile ('./index.html', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  htmlFile = data;
+});
+
+fs.readFile ('./main.css', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  cssFile = data;
+});
+fs.readFile ('./main.js', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  mainJS = data;
+});
+fs.readFile ('./receiver.js', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  receiverJS = data;
+});
+fs.readFile ('./sender.js', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  senderJS = data;
+});
+fs.readFile ('./server.js', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  serverJS = data;
+});
+const options = {
+  key: fs.readFileSync('./certs/key.pem'),
+  cert: fs.readFileSync('./certs/cert.pem')
+};
+const server = https.createServer(options,(request, response) => {
+  switch (request.url) {
+      case '/main.css':
+        response.writeHead (200, {'Content-Type': 'text/css'});
+        response.write (cssFile);
+        break;
+      case '/main.js':
+        response.writeHead (200, {'Content-Type': 'text/javascript'});
+        response.write (mainJS);
+        break;
+      case '/receiver.js':
+        response.writeHead (200, {'Content-Type': 'text/javascript'});
+        response.write (receiverJS);
+        break;
+      case '/sender.js':
+        response.writeHead (200, {'Content-Type': 'text/javascript'});
+        response.write (senderJS);
+        break;
+      case '/server.js':
+        response.writeHead (200, {'Content-Type': 'text/javascript'});
+        response.write (serverJS);
+        break;
+
+      default:
+        response.writeHead (200, {'Content-Type': 'text/html'});
+        response.write (htmlFile);
+    }
+    response.end ();
+});
+server.listen(3000, function() {
+  log("Server is listening on port 3000");
+});
+// Output logging information to console
+
+function log(text) {
+  var time = new Date();
+
+  console.log("[" + time.toLocaleTimeString() + "] " + text);
 }
-const handleSignallingData = (data) => {
-  switch (data.type) {
-    case "answer":
-      peerConnection.setRemoteDescription(data.answer)
-      break
-    case "candidate":
-      peerConnection.addIceCandidate(data.candidate)
+
+// If you want to implement support for blocking specific origins, this is
+// where you do it. Just return false to refuse WebSocket connections given
+// the specified origin.
+function originIsAllowed(origin) {
+  return true;    // We will accept all connections
+}
+
+// Scans the list of users and see if the specified name is unique. If it is,
+// return true. Otherwise, returns false. We want all users to have unique
+// names.
+function isUsernameUnique(name) {
+  var isUnique = true;
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    if (connectionArray[i].username === name) {
+      isUnique = false;
+      break;
+    }
+  }
+  return isUnique;
+}
+
+// Sends a message (which is already stringified JSON) to a single
+// user, given their username. We use this for the WebRTC signaling,
+// and we could use it for private text messaging.
+function sendToOneUser(target, msgString) {
+  var isUnique = true;
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    if (connectionArray[i].username === target) {
+      connectionArray[i].sendUTF(msgString);
+      break;
+    }
   }
 }
-let username;
 
-function sendUsername() {
-  username = document.getElementById('username-input').value;
-  sendData({
-    type: "store_user"
-  });
-}
+// Scan the list of connections and return the one for the specified
+// clientID. Each login gets an ID that doesn't change during the session,
+// so it can be tracked across username changes.
+function getConnectionForID(id) {
+  var connect = null;
+  var i;
 
-function sendData(data) {
-  data.username = username;
-  webSocket.send(JSON.stringify(data));
-  console.log('data',data)
-}
-let localStream;
-let peerConnection;
-
-function startCall() {
-  document.getElementById('video-call-div').style.display = 'inline';
-  navigator.getUserMedia = (
-    navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia
-);
-if(navigator.getUserMedia){
-  navigator.getUserMedia({
-      video: {
-        frameRate: 24,
-        width: {
-          min: 480,
-          max: 1280,
-          ideal: 720
-        },
-        aspectRatio: 1.333
-      },
-      audio: true,
-    },
-    (stream) => {
-      localStream = stream;
-      document.getElementById('local_video').srcObject = localStream;
-      let configuration = {
-        "iceServers": [{
-          "url":'stun:stun2.l.google.com:19302'
-        }]
-
-
-      };
-      peerConnection = new RTCPeerConnection(configuration);
-      peerConnection.addEventListener("addstream", ev => doAddStream(ev.localStream), false);
-      peerConnection.onaddstream = e => {
-        document.getElementById('remote_video').srcObject = e.stream;
-      };
-      peerConnection.onicecandidate = ((e) => {
-        if (e.candidate)
-          return
-        sendData({
-          type: "store_candidate",
-          candidate: e.candidate
-        })
-      })
-      createAndSendOffer();
-    },
-    error => {
-      console.log(error);
+  for (i=0; i<connectionArray.length; i++) {
+    if (connectionArray[i].clientID === id) {
+      connect = connectionArray[i];
+      break;
     }
-  );
-}else {
-  console.log("getUserMedia not supported");
-}
-}
+  }
 
-function createAndSendOffer() {
-  peerConnection.createOffer((offer) => {
-    sendData({
-      type: "store_offer",
-      offer: offer,
-
-    }, (err) => {
-      console.log(err)
-    })
-    console.log('offer',offer)
-  })
+  return connect;
 }
 
-let isAudio = true
+// Builds a message object of type "userlist" which contains the names of
+// all connected users. Used to ramp up newly logged-in users and,
+// inefficiently, to handle name change notifications.
+function makeUserListMessage() {
+  var userListMsg = {
+    type: "userlist",
+    users: []
+  };
+  var i;
 
-function muteAudio() {
-  isAudio = !isAudio
-  localStream.getAudioTracks()[0].enabled = isAudio
-}
-let isVideo = true
+  // Add the users to the list
 
-function muteVideo() {
-  isvideo = !isVideo
-  localStream.getVideoTrack()[0] = isVideo
+  for (i=0; i<connectionArray.length; i++) {
+    userListMsg.users.push(connectionArray[i].username);
+  }
+
+  return userListMsg;
 }
+
+// Sends a "userlist" message to all chat members. This is a cheesy way
+// to ensure that every join/drop is reflected everywhere. It would be more
+// efficient to send simple join/drop messages to each user, but this is
+// good enough for this simple example.
+function sendUserListToAll() {
+  var userListMsg = makeUserListMessage();
+  var userListMsgStr = JSON.stringify(userListMsg);
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    connectionArray[i].sendUTF(userListMsgStr);
+  }
+}
+
+
+// Create the WebSocket server by converting the HTTPS server into one.
+
+var wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false
+});
+
+if (!wsServer) {
+  log("ERROR: Unable to create WbeSocket server!");
+}
+
+// Set up a "connect" message handler on our WebSocket server. This is
+// called whenever a user connects to the server's port using the
+// WebSocket protocol.
+
+wsServer.on('request', function(request) {
+  if (!originIsAllowed(request.origin)) {
+    request.reject();
+    log("Connection from " + request.origin + " rejected.");
+    return;
+  }
+
+  // Accept the request and get a connection.
+
+  var connection = request.accept("json", request.origin);
+
+  // Add the new connection to our list of connections.
+
+  log("Connection accepted from " + connection.remoteAddress + ".");
+  connectionArray.push(connection);
+
+  connection.clientID = nextID;
+  nextID++;
+
+  // Send the new client its token; it send back a "username" message to
+  // tell us what username they want to use.
+
+  var msg = {
+    type: "id",
+    id: connection.clientID
+  };
+  connection.sendUTF(JSON.stringify(msg));
+
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      log("Received Message: " + message.utf8Data);
+
+      // Process incoming data.
+
+      var sendToClients = true;
+      msg = JSON.parse(message.utf8Data);
+      var connect = getConnectionForID(msg.id);
+
+      // Take a look at the incoming object and act on it based
+      // on its type. Unknown message types are passed through,
+      // since they may be used to implement client-side features.
+      // Messages with a "target" property are sent only to a user
+      // by that name.
+
+      switch(msg.type) {
+        // Public, textual message
+        case "message":
+          msg.name = connect.username;
+          msg.text = msg.text.replace(/(<([^>]+)>)/ig, "");
+          break;
+
+        // Username change
+        case "username":
+          var nameChanged = false;
+          var origName = msg.name;
+
+          // Ensure the name is unique by appending a number to it
+          // if it's not; keep trying that until it works.
+          while (!isUsernameUnique(msg.name)) {
+            msg.name = origName + appendToMakeUnique;
+            appendToMakeUnique++;
+            nameChanged = true;
+          }
+
+          // If the name had to be changed, we send a "rejectusername"
+          // message back to the user so they know their name has been
+          // altered by the server.
+          if (nameChanged) {
+            var changeMsg = {
+              id: msg.id,
+              type: "rejectusername",
+              name: msg.name
+            };
+            connect.sendUTF(JSON.stringify(changeMsg));
+          }
+
+          // Set this connection's final username and send out the
+          // updated user list to all users. Yeah, we're sending a full
+          // list instead of just updating. It's horribly inefficient
+          // but this is a demo. Don't do this in a real app.
+          connect.username = msg.name;
+          sendUserListToAll();
+          sendToClients = false;  // We already sent the proper responses
+          break;
+      }
+      if (sendToClients) {
+        var msgString = JSON.stringify(msg);
+        var i;
+        if (msg.target && msg.target !== undefined && msg.target.length !== 0) {
+          sendToOneUser(msg.target, msgString);
+        } else {
+          for (i=0; i<connectionArray.length; i++) {
+            connectionArray[i].sendUTF(msgString);
+          }
+        }
+      }
+    }
+  });
+  connection.on('close', function(reason, description) {
+    // First, remove the connection from the list of connections.
+    connectionArray = connectionArray.filter(function(el, idx, ar) {
+      return el.connected;
+    });
+
+    // Now send the updated user list. Again, please don't do this in a
+    // real application. Your users won't like you very much.
+    sendUserListToAll();
+
+    // Build and output log output for close information.
+
+    var logMessage = "Connection closed: " + connection.remoteAddress + " (" +
+                     reason;
+    if (description !== null && description.length !== 0) {
+      logMessage += ": " + description;
+    }
+    logMessage += ")";
+    log(logMessage);
+  });
+});
